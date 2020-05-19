1a229b4488256b9e0f0a36fbd9f8e4d92b38140c
nalmt <nabil.lamrabet@pm.me>
2020-05-19 16:07:57 +0200

    Add gain function (wip)

--- Projet-Troll-Chateaux/src/main/java/Controller/PlayerController.java
+++ Projet-Troll-Chateaux/src/main/java/Controller/PlayerController.java
@@ -25,4 +25,30 @@ public class PlayerController {
         this.p.setNbStone(this.p.getNbStone() - nbStoneSent);
         return nbStoneSent;
     }
+
+    public int gain(int x, int y, int t) {
+        if (t == 0) {
+            return 0;
+        } else if (x == 0) {
+            return Integer.compare(t, y);
+
+        } else if (y == 0) {
+            return Integer.compare(t, x);
+
+        } else {
+            int[][] matrix = new int[x][y];
+
+            for(int i = 0; i<x ; i++) {
+                for(int j = 0; j<y ; j++) {
+                    matrix[i][j] = gain(i, j, t);
+                    // System.out.println("x : "+ i + "y : " + j + "t : " + t);
+                    // System.out.println(Arrays.deepToString(matrix));
+                }
+            }
+
+            //TODO à partir de matrix faire un pl et retourner la valeur
+            // pour l'instant on retourne matrix[0][0], c'est juste histoire de retourner un truc
+            return matrix[0][0];
+        }
+    }
 }
--- Projet-Troll-Chateaux/src/main/java/com/mycompany/projet/troll/chateaux/MainApp.java
+++ Projet-Troll-Chateaux/src/main/java/com/mycompany/projet/troll/chateaux/MainApp.java
@@ -1,6 +1,7 @@
 package com.mycompany.projet.troll.chateaux;
 
 import Controller.GameController;
+import Controller.PlayerController;
 import Model.Castle;
 import Model.Game;
 import Model.Map;
@@ -8,36 +9,32 @@ import Model.Player;
 import Model.Troll;
 import View.MainView;
 
-import static javafx.application.Application.launch;
-import javafx.application.Application;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class MainApp extends Application {
+public class MainApp {
 
     public static void main(String[] args) {
+
+
         Map map = new Map(3); //Size = 3*2 + 3 = 9
         
         Castle c1 = new Castle(1, map.getCases().get(0)); //One castle on the first case.
-        Castle c2 = new Castle(1, map.getCases().get(map.getCases().size()-1)); //One castle on the last case.
+
+       Castle c2 = new Castle(1, map.getCases().get(map.getCases().size()-1)); //One castle on the last case.
         
         Player J1 = new Player(1,15,c1);
         Player J2 = new Player(2,15,c2);
         
         Troll troll = new Troll(0);
-        
+
         Game g = new Game(map, J1,J2, troll);
         GameController gc = new GameController(g);
         
         System.out.println("Step 0 :\n" + g + "\n");
-        
-        gc.playOneGame();
+
+        // gc.playOneGame();
+
+        PlayerController pj = new PlayerController(J1);
+        pj.gain(3, 3, 3);
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {  
-        
-    }
 
 }
